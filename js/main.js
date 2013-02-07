4b5ce52be184cf4ecd0e5b9028f834c6bb11e383
Kibotu <cloudgazer3d@gmail.com>
2013-02-07 13:16:10 +0100

    bla ...

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 $(document).ready(function () {
 	
 	// define all slides
-	var slides = [ 	"news", "twitter", "mensa", "donations", "member"];
+	var slides = [ 	{"id" : "news", "time" : 10000}, {"id" : "twitter", "time" : 10000}, {"id" : "mensa", "time" : 10000}, {"id" : "donations", "time" : 5000}, {"id" : "member", "time" : 5000}, {"id" : "comic", "time" : 15000}];
 	
 	// define all feed urls
 	var feeds = { 	"infoini_news":"http://www.infoini.de/redmine/projects/fsropen/news.atom", 
 					"infoini_status" : "http://www.infoini.de/status.json",
 					"mensa" : "http://www.studentenwerk-berlin.de/speiseplan/rss/beuth/tag/lang/0",
 					"mensa_ini": "http://infoini.de/mensa.xml",
-					"twitter" : ""
+					"twitter" : "infoini",
+					"xkcd" : "http://xkcd.com/rss.xml",
+					"xkcd_ini" : "http://infoini.de/xkcd.xml"
+	};
+	
+	var getTimes = function() {
+		var counter = 0;
+		for(i in slides) {
+			counter += slides[i].time;
+		}
+		return counter;
 	};
 
 	/** CONSTANTS (no magic numbers :) ) **/
-	var SLIDE_INTERVAL = 10000;
 	var INI_STATUS_INTERVAL = 1000;
 	var DEFAULT_STARTING_SLIDE = 0;
 	var FEED_UPDATE_INTERVAL = 10000;
-	var PAGE_REFRESH_INTEVAL = 10000;
+	var PAGE_REFRESH_INTEVAL = getTimes();
 	var ENABLE_SLIDESHOW = true;
 	var DEBUG_MODUS = false;
+	
+	var log = function(msg) {
+		if(DEBUG_MODUS) console.log(msg);
+	};
 	
 	// days of the week
     var weekdays = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
@@ -30,7 +43,7 @@ $(document).ready(function () {
 	
 	// handle ini status : cafe, time, potentielly door
     var iniStatusHandler = function (data, textStatus) {
-        console.log("onSuccess_status");
+        log("onSuccess_status");
         //$('.tuer').text(data.tuer_offen?'offen':'geschlossen');
 		//data = jQuery.parseJSON(data); for firefox, no need for chrome for some weird reason
         var kanne1_lvl = data.cafepots[0].level;
@@ -50,7 +63,7 @@ $(document).ready(function () {
 
 	// handle infoini.de news
     var newsHandler = function (data, textStatus) {
-        console.log("onSuccess_news");
+        log("onSuccess_news");
         var items = [];
         var $xml = $(data);
         $xml.find("entry").each(function () {
@@ -76,7 +89,7 @@ $(document).ready(function () {
 	
 	// handle mensa feed
 	var mensaHandler = function (data, textStatus) {
-        console.log("onSuccess Mensa");
+        log("onSuccess Mensa");
 		
 		// xml to html and hide it so we can use jquery selectors; TODO refactor to parse xml directly
         var $xml = $(data);
@@ -111,26 +124,52 @@ $(document).ready(function () {
 			$('#food').append(((i%2 == 0) ? leftSideDiv : rightSideDiv) + "<h2>" + items[i].title + '</h2>' + items[i].food.text().replace(/[0-9]/g, '').replace(/\n/g, "<br />") + '</div>');
 		}
 	};
+	
+	// handle mensa feed
+	var xkcdHandler = function (data, textStatus) {
+        log("onSuccess xkcd");
+				
+		// random number in range
+		var randomNumber = function getRandomInt (min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		};
+				
+		var items = [];
+		var appendstuff = function() {
+			// append to content
+			$("#comic-content").html('');
+			// get all comic items (first element is headline)
+			var item = items[randomNumber(1,items.length)];
+			$("#comic-content").append(item + "<br />");
+			$("#comic-content").append($(item).attr("title"));
+		};
+        var $xml = $(data);
+		$xml.find("description").each(function () {
+			items.push($(this).text());
+		});
+		appendstuff();
+	};
 
 	/** UPDATE FEEDS **/
     var getUpdate = function () {
-        console.log('getupdate');
+        log('getupdate');
         $.get(feeds.infoini_news, newsHandler);
         $.ajax(feeds.infoini_status, {success: iniStatusHandler, cache: false, ifModified: false});
 		$.get(feeds.mensa_ini, mensaHandler);
+		$.get(feeds.xkcd_ini, xkcdHandler);
     };
     getUpdate();
     setInterval(getUpdate, FEED_UPDATE_INTERVAL);
-    if(DEBUG_MODUS) setTimeout("location.reload(true);", PAGE_REFRESH_INTEVAL);
+    setTimeout("location.reload(true);", PAGE_REFRESH_INTEVAL);
 	
 	/** TWITTER **/
     jQuery(function ($) {
         $("#tweets").tweet({
-            query: 'infoini',
+            query: feeds.twitter,
             avatar_size: 64,
             count: 5,
             loading_text: "loading tweets...",
-            efresh_interval: 30
+            refresh_interval: 30
         });
     });
 
@@ -144,11 +183,11 @@ $(document).ready(function () {
         hours = hours < 10 ? '0' + hours : '' + hours;
         minutes = minutes < 10 ? '0' + minutes : '' + minutes;
 
-        var time = '[ ' + weekdays[date.getDay()] + ', ' + date.getDate() + '.' + date.getMonth() + 1 + '.' + date.getFullYear() + ' ' + hours + (timeSeparator ? ':' : ' ') + minutes + ' ]';
+        var time = '[ ' + weekdays[date.getDay()] + ', ' + date.getDate() + '.' + date.getMonth() + 1 + '.' + date.getFullYear() + ' ' + hours + (timeSeparator ? '<span style="display:inline-block;width:10px;">:</span>' : '<span style="display:inline-block;width:10px;"> </span>') + minutes + ' ]';
         timeSeparator = !timeSeparator;
-        var cafe = '[ CAFE ' + cafe_level + '%] ';
+        var cafe = '[ CAFE ' + cafe_level + '% ] ';
 
-        $('#date').text(cafe + ' ' + time);
+        $('#date').html(cafe + ' ' + time);
     };
     var timeInterval = setInterval(showStatus, INI_STATUS_INTERVAL);
     showStatus();
@@ -158,7 +197,7 @@ $(document).ready(function () {
 	// hide all slides
 	var hideAll = function() {
 		for(var i = 0; i < slides.length; ++i) {
-			$("#"+slides[i]).hide();
+			$("#"+slides[i].id).hide();
 		}
 	};
 	hideAll();
@@ -168,17 +207,20 @@ $(document).ready(function () {
 		// hide all
 		hideAll();
 		// display slide
-		$("#"+slides[pageNumber]).show();
+		$("#"+slides[pageNumber].id).show();
 	};
 	// start with news
 	showSlide(DEFAULT_STARTING_SLIDE);
 	
 	/** AUTOMATIC SLIDES **/
-    var pageCounter = 0;
+    var pageCounter = DEFAULT_STARTING_SLIDE-1; 
     var slidePage = function () {
         pageCounter = (pageCounter + 1 ) % slides.length;
 		if(ENABLE_SLIDESHOW) showSlide(pageCounter);
+		setTimeout(slidePage, slides[pageCounter].time);
     };
-    var pageInterval = setInterval(slidePage, SLIDE_INTERVAL);
+	slidePage();
+	
+    //var pageInterval = setInterval(slidePage, SLIDE_INTERVAL);
     //setTimeout(function() { $.scrollTo(pages[2]);},3000);
 });
